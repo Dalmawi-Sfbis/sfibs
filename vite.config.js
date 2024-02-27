9554c21b4cebde033a33b4f36d1d1cd0bb2b1a27
Dalmawi-Sfbis <website@sfbis.nu>
2024-02-27 17:07:29 +0100

    add homepage

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Dalmawi-Sfbis"
+  base: "dalmawi-sfbis.github.io"
 })
