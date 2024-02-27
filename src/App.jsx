cb818dc4bf8c929d59be2c2f96a7b6497f58a993
Dalmawi-Sfbis <website@sfbis.nu>
2024-02-27 16:44:29 +0100

    sfbis

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import underconstruction from "../public/images/underkonstruktion.jpg"
+export default function App() {
+  return (
+    <div>
+      <img src={underconstruction} alt="" srcset="" />
+    </div>
+  )
+}
